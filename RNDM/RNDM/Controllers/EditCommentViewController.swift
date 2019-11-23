098cb9196859befabea89148a6785f846d688eb6
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-11-23 09:53:16 +0800

    setup viewController for editing comments

---
+++ RNDM/RNDM/Controllers/EditCommentViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EditCommentViewController.swift
+//  RNDM
+//
+//  Created by Stephenson Ang on 11/23/19.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class EditCommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
